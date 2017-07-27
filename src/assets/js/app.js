81b0cf532cbe5294efa32b0ba0d3701fec1d61c5
ncoxkcg <nathan.cox@kochcreativegroup.com>
2017-07-27 11:14:20 -0500

    Image Showcases

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -2,40 +2,65 @@
 
 //Image Showcase
 
+// updated to get list items from transformaiton
+
 (function($) {
+  var imageShowCaseCount = 0;
   var options = [];
-  $( ".text-option" ).each(function(index) {
-    var bg = $(this).find( '.image-container' ).css('background-image');
-    var idval = "text-option-"+index;
-    var setId = $(this).attr('id', idval);;
-    bg = bg.replace('url(','').replace(')','').replace(/\"/gi, "");
-    $(this).find( '.image-container' ).append('<ul class="Image-Showcase-nav"></ul>');
-    options.push(bg);
-  });
-
-  $( ".Image-Showcase-nav" ).each(function() {
-    var thisIs = $(this);
-    $(options).each(function(index){
-      $(thisIs).append('<li id=showcase-'+ index +'><img src="'+ options[index] +'" alt=""></li>');
-      //console.log(index);
-    });
-  });
-
-  $('.text-option').first().addClass("active");
+  var startAt = -1;
   var old = 0;
-  $( "#showcase-"+old ).find("img").css("opacity", 0.5 );
+  var iteration = 0;
+  $( ".module" ).each(function() {
+    iteration++;
+    var modId = this.id;
+    $("#"+modId+" .text-option" ).each(function() {
+      var bg = $(this).find( '.image-container' ).css('background-image');
+      var idval = "text-option-"+(imageShowCaseCount+1);
+      var setId = $(this).attr('id', idval);
+      bg = bg.replace('url(','').replace(')','').replace(/\"/gi, "");
+      $(this).find( '.image-container' ).append('<ul class="Image-Showcase-nav""></ul>');
+      options.push(bg);
+      imageShowCaseCount++;
+      if(startAt == -1){
+        startAt = imageShowCaseCount;
+        old = startAt;
+      }
+    });
+    var thisIs = $("#"+modId+" .Image-Showcase-nav");
+    var startOld = startAt;
+    while (startAt <= options.length){
+      $(thisIs).append('<li id=showcase-'+ startAt +'><img src="'+ options[startAt-1] +'" alt=""></li>');
+      startAt++;
+    }
+    if(iteration==1)
+      $("#"+modId+" .Image-Showcase-nav li").attr('old', 1);
+    else
+      $("#"+modId+" .Image-Showcase-nav li").attr('old', startOld);
+    $('#'+modId+' .text-option').first().addClass("active");
+    $( '#'+modId+' .Image-Showcase-nav li').first().find("img").css("opacity", 0.5 );
+  });
+
+
+}(jQuery));
+
+(function($) {
   $(".Image-Showcase-nav li").click(function(){
+    var old = $(this).attr("old");
     var id=$(this).attr('id');
     id = parseInt(id.slice(-1), 10);
     if(id!=old){
       $( "#text-option-"+old ).removeClass( "active" );
       $( "#showcase-"+old ).find("img").css("opacity", 1 );
-      old=id;
+      $('li[old="'+old+'"]').attr('old', id);
       $( "#text-option-"+id ).addClass( "active" );
       $( "#text-option-"+id ).find( "#showcase-"+id ).find("img").css( "opacity", 0.5  );
     }
-  }); 
+  });
 }(jQuery));
+
+
+
+
 
 //videos append using code ID from jw player - No iframe
 function appendVideo(){
